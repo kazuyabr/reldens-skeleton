9539ebd54a4e67d611460d83ac7a5d0e750f9601
kazuyabr <sulivan.lineage2@gmail.com>
2021-12-23 20:45:29 -0300

    correções feitas para manter o projeto mais facil de ser subido para produção. é necessario revisar comando de exportação na pipeline

---
+++ dist/assets/maps/full.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="full" tilewidth="16" tileheight="16" tilecount="1992" columns="8">
+ <image source="../Pipoya RPG Tileset 16x16/[Base]BaseChip_pipo.png" width="128" height="3984"/>
+</tileset>
