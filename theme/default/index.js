c1d3692a6756657c1b6c621cdfb6187a868ee02f
kazuyabr <sulivan.lineage2@gmail.com>
2021-12-23 18:22:25 -0300

    deixando apenas uma pasta para o projeto

--- theme/default/index.js
+++ theme/default/index.js
@@ -6,26 +6,38 @@
  *
  */
 
-const $ = require('jquery');
-require('jquery-validation');
-const { CustomClasses } = require('../packages/client');
 const { GameManager } = require('reldens/client');
+const { CustomClasses } = require('../packages/client');
+const { GameConst } = require('reldens/packages/game/constants');
 
-$(document).ready(function($){
-
+// @TODO - BETA.17: move everything from this file as part of the core project and include events to manage the theme.
+window.addEventListener('DOMContentLoaded', () => {
     // reldens game:
     let reldens = new GameManager();
+    let dom = reldens.gameDom;
     reldens.setupClasses(CustomClasses);
-    window.reldens = reldens;
+    // debug events (warning! this will output in the console ALL the event listeners and every event fired):
+    // reldens.events.debug = 'all';
+    // @NOTE: at this point you could specify or override a lot of configurations like your server URL.
+    // reldens.serverUrl = 'wss://my-custom-url.com';
+    // replace all the [values] and uncomment to initialize firebase:
 
-    let $register = $('#register_form'),
-        $login = $('#login_form');
+    // client event listener example with version display:
+    reldens.events.on('reldens.afterInitEngineAndStartGame', () => {
+        dom.getElement('#current-version').innerHTML = reldens.config.version+' -';
+    });
 
-    function restartError(submittedForm)
+    let register = dom.getElement('#register_form'),
+        login = dom.getElement('#login_form'),
+        forgot = dom.getElement('#forgot_form'),
+        fullScreen = dom.getElement('.full-screen-btn'),
+        body = dom.getElement('body');
+
+    function resetErrorBlock(submittedForm)
     {
-        let $errorBlock = $(submittedForm).find('.response-error');
-        $(submittedForm).find('input').on('focus', () => {
-            $errorBlock.hide();
+        let $errorBlock = submittedForm.querySelector('.response-error');
+        submittedForm.querySelector('input').addEventListener('focus', () => {
+            $errorBlock.style.display = 'none';
         });
     }
 
@@ -35,60 +47,121 @@ $(document).ready(function($){
         let gameRoom = reldens.joinGame(formData, isNewUser);
         // you can include here the room as parameter:
         gameRoom.then(() => {
-            $('.loading-container').hide();
-            $('.forms-container').detach();
-            $('.game-container').show();
-        }).catch((data) => {
+            dom.getElement('.loading-container').style.display = 'none';
+            dom.getElement('.footer').style.display = 'none';
+            dom.getElement('.forms-container').remove();
+            dom.getElement('.game-container').classList.remove('hidden');
+            fullScreen.style.display = 'block';
+            body.style.background = '#000000';
+            dom.getElement('.content').style.height = '92%';
+            reldens.gameRoom.onMessage(async (message) => {
+                if(message.act === GameConst.CREATE_PLAYER_RESULT && !message.error){
+                    dom.getElement('body').style.overflow = 'hidden';
+                }
+            });
+        }).catch((err) => {
             // @NOTE: game room errors should be always because some wrong login or registration data. For these cases
             // we will check the isNewUser variable to know where display the error.
-            $('.loading-container').hide();
-            $('#'+formData.formId+' .response-error').html(data).show();
+            reldens.submitedForm = false;
+            dom.getElement('.loading-container').style.display = 'none';
+            let errorElement = dom.getElement('#'+formData.formId+' .response-error');
+            if(errorElement){
+                errorElement.innerHTML = err;
+                errorElement.style.display = 'block';
+            }
+            if(formData.formId === 'firebase_login'){
+                reldens.firebase.app.auth().signOut();
+            }
         });
     }
 
-    if($register.length){
-        restartError($register);
-        $register.on('submit', (e) => {
+    // firebase integration:
+    reldens.firebase.resetErrorBlockCallback = resetErrorBlock;
+    reldens.firebase.startGameCallback = startGame;
+    reldens.firebase.startFirebase();
+
+    if(register){
+        resetErrorBlock(register);
+        register.addEventListener('submit', (e) => {
             e.preventDefault();
             // validate form:
-            if(!$register.valid()){
+            if(!register.checkValidity()){
                 return false;
             }
-            $register.find('.loading-container').show();
+            register.querySelector('.loading-container').style.display = 'block';
+            register.querySelector('.loading-container').classList.remove('hidden');
             let formData = {
-                formId: $register.attr('id'),
-                email: $register.find('#reg_email').val(),
-                username: $register.find('#reg_username').val(),
-                password: $register.find('#reg_password').val()
+                formId: register.id,
+                email: register.querySelector('#reg_email').value,
+                username: register.querySelector('#reg_username').value,
+                password: register.querySelector('#reg_password').value
             };
             startGame(formData, true);
         });
-        $register.validate({
-            rules: {
-                reg_re_password: {
-                    equalTo: '#reg_password'
-                }
-            }
-        });
     }
 
-    if($login.length){
-        restartError($login);
-        $login.on('submit', (e) => {
+    if(login){
+        resetErrorBlock(login);
+        login.addEventListener('submit', (e) => {
             e.preventDefault();
-            if(!$login.valid()){
+            if(!login.checkValidity()){
                 return false;
             }
-            $login.find('.loading-container').show();
+            if(reldens.submitedForm){
+                return false;
+            }
+            reldens.submitedForm = true;
+            login.querySelector('.loading-container').style.display = 'block';
+            login.querySelector('.loading-container').classList.remove('hidden');
             let formData = {
-                formId: $login.attr('id'),
-                email: $login.find('#email').val(),
-                username: $login.find('#username').val(),
-                password: $login.find('#password').val()
+                formId: login.id,
+                username: login.querySelector('#username').value,
+                password: login.querySelector('#password').value
             };
             startGame(formData, false);
         });
-        $login.validate();
     }
+
+    if(forgot){
+        resetErrorBlock(forgot);
+        forgot.addEventListener('submit', (e) => {
+            e.preventDefault();
+            if(!forgot.checkValidity()){
+                return false;
+            }
+            forgot.querySelector('.loading-container').style.display = 'block';
+            forgot.querySelector('.loading-container').classList.remove('hidden');
+            let formData = {
+                formId: forgot.id,
+                forgot: true,
+                email: forgot.querySelector('#forgot_email').value
+            };
+            startGame(formData, false);
+        });
+    }
+
+    if(fullScreen){
+        fullScreen.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(document.fullscreenEnabled){
+                document.body.requestFullscreen();
+            }
+            dom.getElement('.header').style.display = 'none';
+            dom.getElement('.footer').style.display = 'none';
+            dom.getElement('.content').style.height = '100%';
+            reldens.gameEngine.updateGameSize(reldens);
+        });
+    }
+
+    // responsive screen behavior:
+    document.addEventListener('fullscreenchange', () => {
+        if(!document.fullscreenElement){
+            dom.getElement('.header').style.display = 'block';
+            dom.getElement('.content').style.height = '84%';
+        }
+    });
+
+    // global access is not actually required, the app can be fully encapsulated, I'm leaving this here for easy tests:
+    window.reldens = reldens;
 
 });
